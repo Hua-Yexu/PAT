79030895217572644818a97d3c8e7d6cb93604cb
Anpotan <38157890+Anpotan@users.noreply.github.com>
2020-05-12 18:50:26 +0800

    Update 1018.c

--- PAT Basic/1018.c
+++ PAT Basic/1018.c
@@ -4,7 +4,7 @@ void prin_max(int A[]);
 
 int main(void)
 {
-    int N, A_win = 0, same = 0;
+    int N, A_win = 0, A_lost = 0;
     int A[3] = {0};//甲B, C, J赢的次数
     int B[3] = {0};//乙B, C, J赢的次数
     char a, b;
@@ -16,26 +16,23 @@ int main(void)
         scanf("%c %c\n", &a, &b);
         if(a == 'B')
         {
-            if(b == 'B')  same++;
             if(b == 'C')  { A[0]++; A_win++; }
-            if(b == 'J')  B[2]++;
+            if(b == 'J')  { B[2]++; A_lost++; }
         }
         if(a == 'C')
         {
-            if(b == 'B')  B[0]++;
-            if(b == 'C')  same++;
+            if(b == 'B')  { B[0]++; A_lost++; }
             if(b == 'J')  { A[1]++; A_win++; }
         }
         if(a == 'J')
         {
             if(b == 'B')  { A[2]++; A_win++; }
-            if(b == 'C')  B[1]++;
-            if(b == 'J')  same++;
+            if(b == 'C')  { B[1]++; A_lost++; }
         }
     }
 
-    printf("%d %d %d\n", A_win, same, N-A_win-same);
-    printf("%d %d %d\n", N-A_win-same, same, A_win);
+    printf("%d %d %d\n", A_win, N-A_win-A_lost, A_lost);
+    printf("%d %d %d\n", A_lost, N-A_win-A_lost, A_win);
     prin_max(A);
     putchar(' ');
     prin_max(B);
