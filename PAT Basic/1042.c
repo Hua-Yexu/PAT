1212eae4d9b19cd0b474492e3877f7a64a8d86cd
Anpotan <38157890+Anpotan@users.noreply.github.com>
2020-07-17 18:48:56 +0800

    Create 1042.c

---
+++ PAT Basic/1042.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    int ascii[128] = {0};
+    char c;
+    int i, max = 0, alph;
+
+    while( (c = getchar()) != '\n')
+    {
+        if(isalpha(c))
+        {
+            c = tolower(c);
+            ascii[c]++;
+        }
+    }
+
+    for(i = (int)('a'); i <= (int)('z'); i++)
+    {
+        if(ascii[i] > max)
+        {
+            max = ascii[i];
+            alph = i;
+        }
+    }
+
+    printf("%c %d", (char)alph, max);
+
+
+    return 0;
+}
