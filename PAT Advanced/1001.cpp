15b221be2858ecbfc9bd7226be049b01b59c92b9
Hua-Yexu <38157890+Hua-Yexu@users.noreply.github.com>
2021-03-31 22:11:54 +0800

    Create 1001.cpp

---
+++ PAT Advanced/1001.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a, b;
+    cin >> a >> b;
+    
+    string s = to_string(a+b);
+    
+    int len = s.length();
+    for(int i = 0, j = 0; i < len; i++, j++)
+    {
+        cout << s[i];
+        if(s[i] == '-') 
+            continue;
+        if((len-i-1) % 3 == 0 & len-i-1 != 0)
+            cout << ",";
+    }
+    
+    return 0;
+}
