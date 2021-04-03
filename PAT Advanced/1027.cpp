7e089f72d0742386089c97781865cc3a3d8f7769
Hua-Yexu <38157890+Hua-Yexu@users.noreply.github.com>
2021-04-03 11:48:55 +0800

    Create 1027.cpp

---
+++ PAT Advanced/1027.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+using namespace std;
+
+int main(void){
+    char c[14] = {"0123456789ABC"};
+    
+    printf("#");
+    for(int i = 0; i < 3; i++){
+        int num;
+        scanf("%d", &num);
+        printf("%c%c", c[num/13], c[num%13]);
+    }
+    
+    return 0;
+}
