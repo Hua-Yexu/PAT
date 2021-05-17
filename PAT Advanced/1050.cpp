7b9b7ccd6ff214ccb4fd6bf2cbb66ee655f50fcf
Hua-Yexu <38157890+Hua-Yexu@users.noreply.github.com>
2021-05-17 22:03:41 +0800

    Create 1050.cpp

---
+++ PAT Advanced/1050.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+    string s1, s2;
+    
+    getline(cin, s1);
+    getline(cin, s2);
+    
+    for(int i = 0; i < s1.size(); i++){
+        if(s2.find(s1[i]) == string::npos)
+            cout << s1[i];
+    }
+    
+    return 0;
+}
